47dc151ea09119db0a8957bdcb2e3c7d0b8428f3
KrishanR-123 <46771395+KrishanR-123@users.noreply.github.com>
2020-05-01 23:25:40 +0530

    Update Java Primality Test.java

--- Java Primality Test.java
+++ Java Primality Test.java
@@ -14,3 +14,34 @@ public class Solution {
         System.out.println(n.isProbablePrime(10) ? "prime" : "not prime");
     }
 }
+
+
+ //or//
+
+package com.company;
+import java.util.*;
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int n,i;
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+
+        if(n<=1)
+        {System.out.println("not prime");}
+
+           else {
+            for (i = 1; i < n; i++) {
+                if (n % i == 0)
+                    System.out.println("not prime");
+
+                else
+                    System.out.println("prime");
+
+                break;
+            }
+        }
+
+    }
+}
