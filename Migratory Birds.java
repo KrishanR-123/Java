7acca0f733aa9fdda0de24636c0b671fdee21ff8
KrishanR-123 <46771395+KrishanR-123@users.noreply.github.com>
2020-05-12 23:03:37 +0530

    Hackerrank solution

--- Migratory Birds.java
+++ Migratory Birds.java
@@ -24,3 +24,14 @@ public class Solution {
     }
   
   
+public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] ar = new int[n];
+        for(int ar_i = 0; ar_i < n; ar_i++){
+            ar[ar_i] = in.nextInt();
+        }
+        int result = migratoryBirds(n, ar);
+        System.out.println(result);
+    }
+}
