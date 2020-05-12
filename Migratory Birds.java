03eb49e649d28df26031344cdd4c128d95a8936c
KrishanR-123 <46771395+KrishanR-123@users.noreply.github.com>
2020-05-12 23:03:21 +0530

    Update Migratory Birds.java

--- Migratory Birds.java
+++ Migratory Birds.java
@@ -4,3 +4,23 @@ import java.text.*;
 import java.math.*;
 import java.util.regex.*;
 
+public class Solution {
+
+    static int migratoryBirds(int n, int[] ar) {
+        int counter[] = new int[6];
+        int result = 1; int max = 0;
+        for (int i = 0; i < n; i++) {
+            counter[ar[i]]++;
+        }
+
+        for (int i = 1; i <= 5; i++) {
+            if (counter[i] > max) {
+                result = i;
+                max = counter[i];
+            }
+        }
+
+        return result;
+    }
+  
+  
