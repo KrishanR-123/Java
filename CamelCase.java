5f917ec235114e905864e85ea5734d2cbc4df05d
KrishanR-123 <46771395+KrishanR-123@users.noreply.github.com>
2020-05-13 11:23:58 +0530

    Hackerrank solution

--- CamelCase.java
+++ CamelCase.java
@@ -4,3 +4,27 @@ import java.text.*;
 import java.math.*;
 import java.util.regex.*;
 
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String s = in.next();
+        
+        if(s.isEmpty())
+        {
+            System.out.println("0");
+            System.exit(0);
+        }
+        
+        int words = 1;
+        
+        for(char letter : s.toCharArray())
+        {
+            if(letter < 91 && letter > 64 )
+            {
+                words++;
+            }
+        }
+        System.out.println(words);
+    }
+}
