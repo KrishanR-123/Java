cfb3223a7e615e1362470a02c2a3686ec7d77ef8
KrishanR-123 <46771395+KrishanR-123@users.noreply.github.com>
2020-05-13 11:14:46 +0530

    Hackerrank solution

--- Sock Merchant.java
+++ Sock Merchant.java
@@ -4,3 +4,30 @@ import java.text.*;
 import java.math.*;
 import java.util.regex.*;
 import java.util.HashMap;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        
+        HashMap inventory = new HashMap<Integer,Integer>();
+        
+        int matchingPairs = 0;
+        
+        for(int i=0; i < n; i++)
+        {
+            int color = in.nextInt();
+            
+                     
+            if(inventory.containsKey(color) && inventory.get(color).equals(new Integer(1)))
+            {
+                inventory.put(color,0);
+                matchingPairs++;
+                continue;
+            }
+            inventory.put(color,1);
+        }
+        System.out.println(matchingPairs);
+    }
+}
