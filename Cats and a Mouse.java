0d9231d04b7c5109c2e986e0a4648caddd5ad8e5
KrishanR-123 <46771395+KrishanR-123@users.noreply.github.com>
2020-06-19 23:54:42 +0530

    Hackerrank solution

--- Cats and a Mouse.java
+++ Cats and a Mouse.java
@@ -22,4 +22,18 @@ public class Solution {
 
         sc.close();
     }
+    
+    static String solve(int x, int y, int z) {
+        int distanceA = Math.abs(x - z);
+        int distanceB = Math.abs(y - z);
+
+        if (distanceA < distanceB) {
+            return "Cat A";
+        } else if (distanceA > distanceB) {
+            return "Cat B";
+        } else {
+            return "Mouse C";
+        }
+    }
+}
 
