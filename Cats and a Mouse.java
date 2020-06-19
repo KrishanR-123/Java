fd73f807e4a015e54c73b6025bd4618e66d212a5
KrishanR-123 <46771395+KrishanR-123@users.noreply.github.com>
2020-06-19 23:54:19 +0530

    Update Cats and a Mouse.java

--- Cats and a Mouse.java
+++ Cats and a Mouse.java
@@ -6,3 +6,20 @@ import java.util.*;
 import java.util.concurrent.*;
 import java.util.regex.*;
 import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int q = sc.nextInt();
+        for (int tc = 0; tc < q; tc++) {
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+            int z = sc.nextInt();
+
+            System.out.println(solve(x, y, z));
+        }
+
+        sc.close();
+    }
+
